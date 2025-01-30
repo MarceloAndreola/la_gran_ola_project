435dcb65aee1b13e01a3ada2d977d166a34338e1
MarceloAndreola <marceloandreola18@gmail.com>
2025-01-30 15:32:23 -0300

    Preparando para GitHub Pages

---
+++ js/about.59139058.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkla_gran_ola_project"]=self["webpackChunkla_gran_ola_project"]||[]).push([[594],{603:function(a,n,r){r.r(n),r.d(n,{default:function(){return l}});var t=r(6768);const u={class:"about"};function e(a,n){return(0,t.uX)(),(0,t.CE)("div",u,n[0]||(n[0]=[(0,t.Lk)("h1",null,"This is an about page",-1)]))}var c=r(1241);const o={},s=(0,c.A)(o,[["render",e]]);var l=s}}]);
+//# sourceMappingURL=about.59139058.js.map
